adb8aac5a46777427e32df3432158e274116db54
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-11-04 13:00:18 +0300

    :hammer: tech icons

--- components/About.js
+++ components/About.js
@@ -3,11 +3,11 @@ const About = () => {
     <section>
       <div className="container mx-auto px-5">
         <div className="w-24 h-1 rounded bg-[#2B83BA] mt-[-50px] mb-10"></div>
-        <h1 className="text-6xl font-mainFont  bg-gradient-to-tr from-[#2B83BA] to-green-100 text-transparent bg-clip-text">
+        <h1 className=" text-4xl md:text-6xl mb-5 font-mainFont  bg-gradient-to-tr from-[#2B83BA] to-green-100 text-transparent bg-clip-text">
           About Me
         </h1>
 
-        <p class="mb-8  text-gray-400 w-[60%] text-2xl leading-relaxed">
+        <p class="mb-8 text-gray-400 w-full md:w-[60%] text-xl md:text-2xl leading-relaxed">
           My name is Abdulrhman Bokhari and I am a <span> Web Developer </span>{' '}
           from Saudi Arabia{' '}
           <img src="/SA.png" className="w-5 h-3 inline-block" /> Makkah, who
---
+++ components/Tech.js
@@ -0,0 +1,49 @@
+const Tech = () => {
+  return (
+    <section>
+      <div className="container mx-auto px-5">
+        <div className="w-24 h-1 rounded bg-[#2B83BA]  mb-10"></div>
+        <h1 className="text-4xl md:text-6xl font-mainFont  bg-gradient-to-tr from-[#2B83BA] to-green-100 text-transparent bg-clip-text">
+          Technogogies
+        </h1>
+
+        <div className="grid grid-cols-4 gap-6 py-16 ">
+          <img
+            src="/images/bootstrap.svg"
+            className="md:w-20 md:h-20 w-10 h-10"
+          />
+          <img src="/images/css3.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img src="/images/sass.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img
+            src="/images/tailwind-css-icon.svg"
+            className="md:w-20 md:h-20 w-10 h-10"
+          />
+          <img src="/images/html5.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img
+            src="/images/next-js.svg"
+            className="md:w-20 md:h-20 w-10 h-10"
+          />
+          <img src="/images/react.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img
+            src="/images/javascript.svg"
+            className="md:w-20 md:h-20 w-10 h-10"
+          />
+          <img src="/images/redux.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img src="/images/python.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img src="/images/nodejs.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img
+            src="/images/mongodb.svg"
+            className="md:w-20 md:h-20 w-10 h-10"
+          />
+          <img src="/images/github.svg" className="md:w-20 md:h-20 w-10 h-10" />
+          <img
+            src="/images/expressjs.svg"
+            className="md:w-20 md:h-20 w-10 h-10"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tech;
