50c63d279101e7961a23326248e7aaee206f224a
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-11-05 16:24:27 +0300

    :hammer: framer motion

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,16 @@
+import { motion } from 'framer-motion';
 import BackgroundAnimation from './BackgroundAnimation';
-
 const Hero = () => {
   return (
     <section className="body-font -mt-8">
       <div className="container mx-auto flex px-5 md:py-24 py-16 md:flex-row flex-col items-center relative">
         <div className="lg:flex-grow md:full lg:pr-24  flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-start  z-10">
-          <div className=" mb-4 font-medium text-white space-y-5">
+          <motion.div
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: 1, translateX: 0 }}
+            transition={{ duration: 1.5, ease: 'easeIn' }}
+            className=" mb-4 font-medium text-white space-y-5"
+          >
             <p className="font-sndFont opacity-25"> &#60; h1 &#62; </p>
             <h3 className="text-2xl pl-3 md:text-3xl">Hello 👋</h3>
             <h1 className="text-4xl md:text-5xl pl-3 ">
@@ -15,20 +20,35 @@ const Hero = () => {
               Front End Developer
             </h2>
             <p className="font-sndFont opacity-25">&#60; &#47; h1 &#62; </p>
-          </div>
-          <p className="mb-8 leading-relaxed text-gray-400 text-lg lg:w-[50%]">
+          </motion.div>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeIn' }}
+            className="mb-8 leading-relaxed text-gray-400 text-lg lg:w-[50%]"
+          >
             a web developer focused on building a great web experiences. Coding
             have been my passion since the days I started working with computers
-          </p>
-          <div className="flex justify-center">
+          </motion.p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeIn' }}
+            className="flex justify-center"
+          >
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               <a href="mailto:abdulrhman.bokhari23@gmail.com">Hire Me 🔥</a>
             </button>
-          </div>
+          </motion.div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 absolute right-5 top-24">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, ease: 'easeIn' }}
+          className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 absolute right-5 top-24"
+        >
           <BackgroundAnimation />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
