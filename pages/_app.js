66b81d75666382a441cd7467ff8232d9292a067e
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-11-06 15:57:46 +0300

    :hammer: bug

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
+        id="myscript"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
